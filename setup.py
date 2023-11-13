37c0106be83e5c428cda492eb79df829d388f3f6
Emmanuel Vazquez <emmanuel.vazquez@centralesupelec.fr>
2023-11-13 10:52:11 +0100

    checkpoint before transition to new setup procedure

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='gpmp',
-      version='0.9.10',
+      version='0.9.11',
       author='Emmanuel Vazquez',
       author_email='emmanuel.vazquez@centralesupelec.fr',
       description='GPmp: the Gaussian process micro package',
