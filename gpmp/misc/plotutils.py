d616bd4bee95496f4893cb8e9b833b20245daf78
Emmanuel Vazquez <emmanuel.vazquez@centralesupelec.fr>
2023-11-27 10:01:50 +0100

    plotutils.py: several improvements

--- gpmp/misc/plotutils.py
+++ gpmp/misc/plotutils.py
@@ -5,7 +5,9 @@
 ## --------------------------------------------------------------
 import sys
 import numpy as np
+import scipy.stats as stats
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 from matplotlib import interactive
 
 
@@ -13,16 +15,15 @@ class Figure:
     """
     Figures manager class
 
-	Attributes
+    Attributes
     ----------
 
     Methods
     -------
-    
+
     """
 
-    def __init__(self, nrows=1, ncols=1, isinteractive=True, **kargs):
-
+    def __init__(self, nrows=1, ncols=1, isinteractive=True, boxoff=True, **kargs):
         # Check if we run in interpreter mode
         # https://stackoverflow.com/questions/2356399/tell-if-python-is-in-interactive-mode
         self.interpreter = False
@@ -37,26 +38,43 @@ class Figure:
         if isinteractive & self.interpreter:
             interactive(True)
 
+        self.boxoff = boxoff
+
         self.fig = plt.figure(**kargs)
 
         self.nrows = nrows
         self.ncols = ncols
         self.axes = []
-        for i in range(nrows*ncols):
-            self.axes.append(self.fig.add_subplot(nrows, ncols, i+1))
+        for i in range(nrows * ncols):
+            self.axes.append(self.fig.add_subplot(nrows, ncols, i + 1))
         self.ax = self.axes[0]
+        if self.boxoff:
+            self.set_boxoff()
+
+    def set_boxoff(self):
+        self.ax.spines["right"].set_visible(False)
+        self.ax.spines["top"].set_visible(False)
+        self.ax.tick_params(direction="in")
 
     def subplot(self, i):
-        self.ax = self.axes[i-1]
+        self.ax = self.axes[i - 1]
+        if self.boxoff:
+            set_boxoff()
 
-    def show(self):
+    def show(self, grid=None, legend=None, legend_fontsize=None):
+        if grid:
+            self.grid()
+        if legend_fontsize is not None:
+            self.legend(fontsize=legend_fontsize)
+        elif legend:
+            self.legend()
         plt.show()
 
     def plot(self, x, z, *args, **kargs):
         self.ax.plot(x, z, *args, **kargs)
 
     def plotdata(self, x, z):
-        self.ax.plot(x, z,'rs', markerfacecolor='none', markersize=6)
+        self.ax.plot(x, z, "rs", markerfacecolor="none", markersize=6)
 
     def xlabel(self, s):
         self.ax.set_xlabel(s)
@@ -64,81 +82,144 @@ class Figure:
     def ylabel(self, s):
         self.ax.set_ylabel(s)
 
+    def xylabels(self, sx="", sy=""):
+        self.ax.set_xlabel(sx)
+        self.ax.set_ylabel(sy)
+
     def title(self, s):
         self.ax.set_title(s)
 
-    def legend(self):
-        self.ax.legend()
+    def legend(self, **kwargs):
+        self.ax.legend(**kwargs)
 
-    def plotgp(self,
-               x,
-               mean,
-               variance,
-               colorscheme='default',
-               rgb_hue=None,
-               ax=None,
-               fignum=None,
-               **kwargs):
+    def grid(
+        self,
+        visible=True,
+        which="major",
+        linestyle=(0, (1, 5)),
+        linewidth=0.5,
+        **kwargs
+    ):
+        self.ax.grid(visible, which, linestyle=linestyle, linewidth=linewidth, **kwargs)
+
+    def xlim(self, new_limits=None):
+        if new_limits is None:
+            return self.ax.get_xlim()
+        else:
+            self.ax.set_xlim(new_limits)
+            return new_limits
+
+    def ylim(self, new_limits=None):
+        if new_limits is None:
+            return self.ax.get_ylim()
+        else:
+            self.ax.set_ylim(new_limits)
+            return new_limits
+
+    def plotgp(
+        self,
+        x,
+        mean,
+        variance,
+        colorscheme="default",
+        rgb_hue=None,
+        ax=None,
+        fignum=None,
+        mean_label="posterior mean",
+        show_mean_label=True,
+        ci=[0.95, 0.99, 0.999],  # CI levels
+        ci_labels=["CI 95%", "CI 99%", "CI 99.9%"],
+        show_ci_labels=True,
+        **kwargs
+    ):
+        """coverage intervals
+        norminv (1 - 0.05/2)  = 1.959964
+        norminv (1 - 0.01/2)  = 2.575829
+        norminv (1 - 0.001/2) = 3.290527
+        """
+        if not show_mean_label:
+            mean_label = ""
+        if not show_ci_labels:
+            ci_labels = [""] * 3
+
         mean = mean.flatten()
         x = x.flatten()
 
-        ''' coverage intervals
-           norminv (1 - 0.05/2)  = 1.959964
-           norminv (1 - 0.01/2)  = 2.575829
-           norminv (1 - 0.001/2) = 3.290527
-        '''
-        if colorscheme == 'hue':
-            hex_code = '#' + ''.join([format(i, '02x') for i in rgb_hue])
+        # Compute delta values using scipy.stats.norm.ppf for the given CI levels
+        delta0 = [stats.norm.ppf((1 + level) / 2) for level in ci]
+
+        if colorscheme == "hue":
+            hex_code = "#" + "".join([format(i, "02x") for i in rgb_hue])
             mcol = hex_code
             mwidth = 2.0
-            delta0 = [ 1.959964 ]
-            fillcol = [ hex_code ]
+            delta0 = [delta0[0]]
+            ci_labels = [ci_labels[0]]
+            fillcol = [hex_code]
             alpha = 0.5
-            kwargs['linewidth'] = 0.5
-            drawulb=False
-        if colorscheme == 'bw':
-            mcol = '#000000'
+            kwargs["linewidth"] = 0.5
+            drawulb = False
+        if colorscheme == "bw":
+            mcol = "#000000"
             mwidth = 2.0
-            edgecol='#000000'
-            delta0 = [ 1.959964 ]
-            fillcol = [ '#F2F2F2' ]
+            edgecol = "#000000"
+            delta0 = [delta0[0]]
+            ci_labels = [ci_labels[0]]
+            fillcol = ["#F2F2F2"]
             alpha = 0.0
-            drawulb=True
-        if colorscheme == 'simple':
-            mcol = '#F2404C'
+            drawulb = True
+        if colorscheme == "simple":
+            mcol = "#F2404C"
             mwidth = 2.0
-            delta0 = [ 1.959964 ]
-            fillcol = [ '#BFBFBF' ]
+            delta0 = [delta0[0]]
+            ci_labels = [ci_labels[0]]
+            fillcol = ["#BFBFBF"]
             alpha = 0.8
-            kwargs['linewidth'] = 0.5
-            drawulb=False
-        if colorscheme == 'default':
-            mcol = '#F2404C'
+            kwargs["linewidth"] = 0.5
+            drawulb = False
+        if colorscheme == "default":
+            mcol = "#F2404C"
             mwidth = 2.0
-            delta0 = [ 3.290527, 2.575829, 1.959964 ]
-            fillcol = [ '#F2F2F2', '#D8D8D8', '#BFBFBF' ]
+            delta0 = delta0[::-1]
+            ci_labels = ci_labels[::-1]
+            fillcol = ["#F2F2F2", "#D8D8D8", "#BFBFBF"]
             alpha = 0.8
-            kwargs['linewidth'] = 0.5
-            drawulb=False
+            kwargs["linewidth"] = 0.5
+            drawulb = False
 
-            
         # mean
-        self.ax.plot(x, mean, mcol, linewidth=mwidth)
-            
-        for i in range(len(delta0)):
-            kwargs['alpha'] = alpha
+        self.ax.plot(x, mean, mcol, linewidth=mwidth, label=mean_label)
 
-            lower = mean - delta0[i] * np.sqrt(variance.flatten())
-            upper = mean + delta0[i] * np.sqrt(variance.flatten())
+        for i, delta in enumerate(delta0):
+            kwargs["alpha"] = alpha
 
-            self.ax.fill(np.hstack((x, x[::-1])),
-                         np.hstack((upper, lower[::-1])),
-                         color=fillcol[i],
-                         **kwargs)
+            lower = mean - delta * np.sqrt(variance.flatten())
+            upper = mean + delta * np.sqrt(variance.flatten())
+
+            self.ax.fill(
+                np.hstack((x, x[::-1])),
+                np.hstack((upper, lower[::-1])),
+                color=fillcol[i],
+                label=ci_labels[i],
+                **kwargs
+            )
 
             if drawulb:
-                self.ax.plot(x, upper, color=edgecol, linestyle='dashed', dashes=(10, 8), linewidth=0.5)
-                self.ax.plot(x, lower, color=edgecol, linestyle='dashed', dashes=(10, 8), linewidth=0.5)
+                self.ax.plot(
+                    x,
+                    upper,
+                    color=edgecol,
+                    linestyle="dashed",
+                    dashes=(10, 8),
+                    linewidth=0.5,
+                )
+                self.ax.plot(
+                    x,
+                    lower,
+                    color=edgecol,
+                    linestyle="dashed",
+                    dashes=(10, 8),
+                    linewidth=0.5,
+                )
 
 
 def crosssections(model, xi, zi, box, ind_i, ind_dim, nt=100):
@@ -170,20 +251,25 @@ def crosssections(model, xi, zi, box, ind_i, ind_dim, nt=100):
 
     for i in range(num_crosssections):
         for d in range(num_dims):
-            t = np.sort(np.concatenate((
-                np.linspace(box[0, d], box[1, d], nt-1),
-                np.array([xi[ind_i[i], ind_dim[d]]]))))
+            t = np.sort(
+                np.concatenate(
+                    (
+                        np.linspace(box[0, d], box[1, d], nt - 1),
+                        np.array([xi[ind_i[i], ind_dim[d]]]),
+                    )
+                )
+            )
             xt = np.tile(xi[ind_i[i], :], (nt, 1))
             xt[:, ind_dim[d]] = t
             (zpm, zpv) = model.predict(xi, zi, xt)
             zpv = np.maximum(zpv, 0)
             fig.subplot(num_crosssections * d + i + 1)
             fig.plotgp(t, zpm, zpv)
-            fig.plot(xi[ind_i[i], ind_dim[d]]*np.array([1, 1]), fig.ax.get_ylim())
+            fig.plot(xi[ind_i[i], ind_dim[d]] * np.array([1, 1]), fig.ax.get_ylim())
             if i == 0:
-                fig.ax.set_ylabel('z along x_{:d}'.format(d+1))
+                fig.ax.set_ylabel("z along x_{:d}".format(d + 1))
             if d == 0:
-                fig.ax.set_title('cross section {:d}'.format(i+1))
+                fig.ax.set_title("cross section {:d}".format(i + 1))
 
 
 def plot_loo(zi, zloom, zloov):
@@ -199,11 +285,11 @@ def plot_loo(zi, zloom, zloov):
         _description_
     """
     fig = Figure()
-    fig.ax.errorbar(zi, zloom, 1.96 * np.sqrt(zloov), fmt='ko', ls='None')
-    fig.ax.set_xlabel('true values'), plt.ylabel('predicted')
-    fig.ax.set_title('LOO predictions with 95% coverage intervals')
+    fig.ax.errorbar(zi, zloom, 1.96 * np.sqrt(zloov), fmt="ko", ls="None")
+    fig.ax.set_xlabel("true values"), plt.ylabel("predicted")
+    fig.ax.set_title("LOO predictions with 95% coverage intervals")
     (xmin, xmax), (ymin, ymax) = fig.ax.get_xlim(), fig.ax.get_ylim()
     xmin = min(xmin, ymin)
     xmax = max(xmax, ymax)
-    fig.ax.plot([xmin, xmax], [xmin, xmax], '--')
+    fig.ax.plot([xmin, xmax], [xmin, xmax], "--")
     fig.show()
